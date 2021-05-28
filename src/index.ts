c6c734c9466af3813a4af42a0906f060cc7aa5be
afeiship <1290657123@qq.com>
2021-05-28 19:49:40 +0800

    refactor: inlist rename

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import filterProps from 'filter-react-props';
 
-export default (inProps, inFilterList: string[] = []) => {
+export default (inProps, inArray: string[] = []) => {
   const filtered = filterProps(inProps);
   return Object.keys(filtered)
-    .filter((item) => !inFilterList.includes(item))
+    .filter((item) => !inArray.includes(item))
     .reduce((cleanProps, propName) => {
       cleanProps[propName] = inProps[propName];
       return cleanProps;
